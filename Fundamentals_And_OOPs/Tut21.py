61d200f004c0c18de36b257496fabc47aed4a586
shubhamjaiswal404 <shubhamjaiswal08022003@gmail.com>
2025-11-20 14:22:56 +0530

    Adding Tut21 Question

---
+++ Fundamentals_And_OOPs/Tut21.py
@@ -0,0 +1,17 @@
+#Exercise
+#Health Management System
+#3 Client-Harry , Rohan , and Hammed
+
+"""
+Total 6 file
+write  a function that when executed takes as input client name
+
+def getdate():
+    import datetime
+    return datetime.datetime.now()
+
+    []cable crossover
+
+# One more function to retrive exercise or food for any client
+
+"""
